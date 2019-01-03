8febdfd4e331a512651f4733294fd1f25e9f6887
shangjieZou <31823702+shangjieZou@users.noreply.github.com>
2019-01-03 22:18:01 +0800

    Update Get_information_from_Shixiseng.py

--- Get_information_from_Shixiseng.py
+++ Get_information_from_Shixiseng.py
@@ -11,40 +11,47 @@ def getHTMLText(url):
         return "Error Occured when getting url"
 
 
-def parsePage(list_job, html):
+def parsePage(html):
     try:
         # data-sname="96" data-desc="search-公司名称">深声科技</a>
         # <span class="type">数据库</span>
         # data-desc="search-职位名称">实习&#xe22d（数据审核员）</a>
         Company_pattern = r"data-sname=\"96\" data-desc=\"search-公司名称\">(.+)</a>"
-        Company_name = re.findall(Company_pattern,html)
+        Company_name = str(re.findall(Company_pattern,html))
         Responsibility_pattern = r"<span class=\"type\">(.+)</span>"
-        Responsibility_name = re.findall(Responsibility_pattern,html)
+        Responsibility_name = str(re.findall(Responsibility_pattern,html))
         Job_Title_pattern = r"data-desc=\"search-职位名称\">（.+）</a>"
-        Job_Title = re.findall(Job_Title_pattern,html).strip("&#xe22d")
-        for i in range(len(Company_name)):
-            list_job.append([Company_name,Responsibility_name,Job_Title])
+        Job_Title = str(re.findall(Job_Title_pattern,html))
+        Job_Title.strip("&#xe22d")
+        Company_list = Company_name.split(",")
+        Responsibility_list = Responsibility_name.split(",")
+        Job_list = Job_Title.split(",")
+        # use this block to check problem when Error Occured in Parse
+        if(0):
+            print(Company_list)
+            print(len(Company_list))
+            print(Responsibility_list)
+            print(len(Responsibility_list))
+            print(Job_list)
+            print(len(Job_list))
+        for i in range(10):
+            print(Company_list[i]+"  "+Responsibility_list[i])
     except:
         print("Error Occured in Parse")
 
 
-def printJobList(list_job):
-    pass
-
-
 def main():
     job_title = '数据'   # key word for browsing
-    depth = 2   # similar to the number of pages to be scanned
+    depth = 10   # similar to the number of pages to be scanned
     Job_url_start = 'https://www.shixiseng.com/interns/c-440100_?k=' + job_title
-    list_job = []
-    for i in range(depth):
+    for i in range(1,depth+1):
         try:
             url = Job_url_start + '&p=' + str(i)
             html = getHTMLText(url)
-            parsePage(list_job, html)
+            print(url)
+            parsePage(html)
         except:
             continue
-    printJobList(list_job)
 
 
 main()
